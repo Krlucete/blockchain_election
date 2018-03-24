426fee4157edb61105587f874289289fce2b4bd7
Krlucete <Krlucete@gmail.com>
2018-03-24 15:47:45 +0900

    Complete client draft for voters

---
+++ router/auth.js
@@ -0,0 +1,66 @@
+module.exports = function(app) {
+    const express = require('express');
+    const session = require('express-session');
+    const mysql = require('mysql');
+
+    app.get('/auth/logout', function(req, res) {
+      req.session.destroy();
+      res.redirect('/');
+    });
+
+    app.post('/auth/login', function(req, res) {
+        var uname = req.body.username;
+        var pwd = req.body.password;
+        const is_voted = 1;
+        var sql = "select * from citizens where name=\'" + uname + "\' AND resident_num = \'" + pwd + "\'";
+        var conn = mysql.createConnection({
+          host: 'localhost',
+          user: 'root',
+          password: 'j2dxro3r',
+          port: '3307',
+          database: 'voter'
+        });
+        conn.connect();
+
+        conn.query(sql, function(err, rows, fields) {
+
+            if (err) {
+              console.log(err);
+            } else {
+              if (rows == "") {
+                res.render('login_fail/login_fail.html');
+
+              }else{
+                var resident_num = rows[0].resident_num;
+                var first_resident_num = resident_num.substring(0, 1);
+                var age = 0;
+                if (first_resident_num == "0") {
+                  age = 2018 - 2000 - Number(resident_num.substring(0, 2));
+                } else {
+                  age = 2018 - 1900 - Number(resident_num.substring(0, 2));
+                }
+
+                if(rows[0].is_voted!=is_voted){
+                  res.render('login_fail/login_fail_voted.html');
+
+                }else if(age < 20) {
+                  res.render('login_fail/login_fail_age.html');
+                } else {
+                  var id = rows[0].id;
+                  var name = rows[0].name;
+
+                  req.session.username = name;
+                  req.session.userid = id;
+                  req.session.save(function() {
+                    res.render('vote.html');
+                  });
+                }
+              }
+            }
+        });
+      });
+
+      app.get('/auth/login', function(req, res) {
+        res.render('login.html');
+      });
+    };
---
+++ router/main.js
@@ -0,0 +1,69 @@
+module.exports = function(app) {
+  const session = require('express-session');
+  const bodyParser = require('body-parser');
+  const MySQLStore = require('express-mysql-session')(session);
+  const mysql = require('mysql');
+
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+  app.use(bodyParser.json());
+
+  app.get('/', function(req, res) {
+    res.render('start.html');
+  });
+  app.get('/test', function(req, res) {
+    res.render('test.html');
+  });
+  app.get('/select_page', function(req, res) {
+    res.render('select_page.html');
+  });
+
+  app.get('/presidential_election', function(req, res) {
+    if (req.session.username) {
+      res.render('vote.html');
+    } else {
+      res.render('login_fail/login_fail_null.html');
+    }
+  });
+  app.get('/shareholders_meeting', function(req, res) {
+    res.send(`
+      <h1>shareholders_meeting.</h1>
+    `);
+  });
+  app.post('/try_voting', function(req, res) {
+    if(req.session.userid != undefined && req.session.username != undefined){
+      console.log(req.session.userid + "인 " + req.session.username + "가 "+req.body.label + "에게 투표했습니다.");
+      res.render("vote.html");
+    }else{
+      console.log("투표권이 만료되었습니다. 다시 확인해 주십시오.");
+      res.render('login_fail/login_fail.html');
+    }
+
+  });
+  app.get('/vote_success', function(req, res) {
+    var sql = "UPDATE citizens SET is_voted = 0 WHERE id = \'" + req.session.userid + "\'";
+
+    var conn = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'j2dxro3r',
+      port: '3307',
+      database: 'voter'
+    });
+    conn.connect();
+    conn.query(sql, function(err) {
+        if (err) {
+          console.log(err);
+        }
+    });
+    req.session.destroy();
+    res.render('vote_success.html');
+  });
+  app.get('/voter_info', function(req, res) {
+    res.render('voter_info.html');
+  });
+  app.get('/test', function(req, res) {
+    res.render('test.html');
+  });
+}
