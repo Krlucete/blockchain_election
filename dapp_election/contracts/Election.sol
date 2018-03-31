588bfb539dac1678ef5e335c96853eb903977748
Krlucete <Krlucete@gmail.com>
2018-03-31 15:40:26 +0900

    Build environment for contract development 1. Install Truffle, Ganache, METAMASK(chrome extension) 2. Simple Voting web page implementation for testing(No voting feature implemented)

---
+++ dapp_election/contracts/Election.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.19;
+
+contract Election {
+	// Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	// Store Candidate
+	// Fetch Candidate
+	mapping(uint => Candidate) public candidates;
+	// Store Candidates Count
+
+	uint public candidatesCount;
+
+	// Constructor
+	function Election () public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+	
+	function addCandidate (string _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+}
